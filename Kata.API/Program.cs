69fddaa6d35b44ea3627eb1307402afeb530e809
fuxeen <aymeric.tavernier@gmail.com>
2025-02-07 16:56:05 +0100

    mini commit

--- Kata.API/Program.cs
+++ Kata.API/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddCors(opt => opt.AddPolicy("CorsPolicy", policy =>
 {
     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
 }));
+
 builder.Services.AddTransient<IClanRepository, ClanRepository>();
 builder.Services.AddTransient<IBattleRepository, BattleRepository>();
 builder.Services.AddScoped<IClanService, ClanService>();
